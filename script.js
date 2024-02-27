f6b49c53d301e57de460b925c005e26a6af014fd
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-27 15:58:36 +0100

    fix Script.js Commit

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // PALIDROME EX
-/*
-
 let userWord = prompt('Inserisci una parola'); //Lascio scegliere all'utente la parola
 
 function reverseString(strElement) { //Creo la funzione
     let palindromeUserWord = ''; //Creo una variabile STRINGA vuota
     for (let i = strElement.length - 1; i >= 0; i--) { //CICLO all'interno degli indici della parola scelta partendo da 0 e sottraggo il valore dell'indice ad ogni ciclo, cosi da poter "ribaltare" la parola
         palindromeUserWord += strElement[i] //cambio il valore della variabile con L'indice della parola ad ogni ciclo
-        
+
     }
     return palindromeUserWord //Ritorno la variabile con la parola INVERTITA
 }
@@ -15,7 +13,7 @@ console.log(reverseString(userWord)); //STAMP
 
 console.log(reverseString('! OTUIA')); //STAMP "help"
 
-*/
+
 ///////////////////////////////////////////////////////////////////////
 
 // Pari o Dispari EX
